a20828c1eacde68c097b91c1e7328b4698c0acb7
Quilsen <70715580+Quilsen@users.noreply.github.com>
2025-01-15 23:43:24 +0100

    (FEAT): add filterProducts method with pageable suport to access paginated sorted and filtered data

--- ExternalService/src/main/java/org/example/externalservice/service/ProductService.java
+++ ExternalService/src/main/java/org/example/externalservice/service/ProductService.java
@@ -1,16 +1,21 @@
 package org.example.externalservice.service;
 
 import lombok.AllArgsConstructor;
+import org.example.externalservice.dto.CustomPageImpl;
 import org.example.externalservice.dto.ProductCreateDto;
 import org.example.externalservice.dto.ProductDto;
 import org.example.externalservice.dto.ProductUpdateDto;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @AllArgsConstructor
@@ -51,6 +56,36 @@ public class ProductService {
         return exchange.getBody();
     }
 
+    public Page<ProductDto> filterProducts(Integer page, Integer size, String sort, String name, String description,  BigDecimal price) {
+        UriComponentsBuilder uri = UriComponentsBuilder.fromUriString(PRODUCTS_URL + "/filter");
+        if (page != null) {
+            uri.queryParam("page", page);
+        }
+        if (size != null) {
+            uri.queryParam("size", size);
+        }
+        if (StringUtils.hasText(sort)) {
+            uri.queryParam("sort", sort);
+        }
+        if (StringUtils.hasText(name)) {
+            uri.queryParam("name", name);
+        }
+        if (StringUtils.hasText(description)) {
+            uri.queryParam("description", description);
+        }
+        if (price != null) {
+            uri.queryParam("price", price);
+        }
+
+        ResponseEntity<CustomPageImpl<ProductDto>> exchange = restTemplate.exchange(
+                uri.toUriString(),
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<CustomPageImpl<ProductDto>>() {}
+        );
+       return exchange.getBody();
+    }
+
     public ProductDto updateProduct(Long id, ProductUpdateDto productUpdateDto) {
         ResponseEntity<ProductDto> exchange = restTemplate.exchange(
                 PRODUCTS_URL + "/" + id,
@@ -64,5 +99,4 @@ public class ProductService {
     public void deleteProduct(Long id) {
         restTemplate.delete(PRODUCTS_URL + "/" + id);
     }
-
 }
