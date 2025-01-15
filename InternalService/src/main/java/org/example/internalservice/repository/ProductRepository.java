b04f7dff56b54b8b03878ad29f330bf137288b32
Quilsen <70715580+Quilsen@users.noreply.github.com>
2025-01-15 23:35:29 +0100

    (FEAT): add JpaSecification to ProductRepository to use Specification

---
+++ InternalService/src/main/java/org/example/internalservice/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package org.example.internalservice.repository;
+
+import org.example.internalservice.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+}
--- InternalService/src/main/java/org/example/internalservice/service/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.internalservice.service;
-
-import org.example.internalservice.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
