8d74de892b29810d194361cd651322eec4552900
Quilsen <70715580+Quilsen@users.noreply.github.com>
2025-01-15 12:01:19 +0100

    (FEAT): create productService with CRUD operations

---
+++ InternalService/src/main/java/org/example/internalservice/service/ProductService.java
@@ -0,0 +1,55 @@
+package org.example.internalservice.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import org.example.internalservice.dto.ProductCreateDto;
+import org.example.internalservice.dto.ProductDto;
+import org.example.internalservice.dto.ProductUpdateDto;
+import org.example.internalservice.exception.ProductNotFoundException;
+import org.example.internalservice.model.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class ProductService {
+    private static final String PRODUCT_NOT_FOUND_MSG = "Product not found with id: ";
+    private final ProductRepository productRepository;
+    private ObjectMapper objectMapper;
+
+    public ProductDto createProduct(ProductCreateDto productCreateDto) {
+        Product product = objectMapper.convertValue(productCreateDto, Product.class);
+        Product save = productRepository.save(product);
+        return objectMapper.convertValue(save, ProductDto.class);
+    }
+
+    public ProductDto getProduct(Long id) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new ProductNotFoundException( PRODUCT_NOT_FOUND_MSG + id));
+        return objectMapper.convertValue(product, ProductDto.class);
+    }
+
+    public List<ProductDto> getAllProducts() {
+        return productRepository.findAll()
+                .stream()
+                .map(product -> objectMapper.convertValue(product, ProductDto.class))
+                .toList();
+    }
+
+    public ProductDto updateProduct(Long id, ProductUpdateDto productUpdateDto) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new ProductNotFoundException(PRODUCT_NOT_FOUND_MSG + id));
+
+        Product updatedProduct = objectMapper.convertValue(productUpdateDto, product.getClass());
+        Product save = productRepository.save(updatedProduct);
+        return objectMapper.convertValue(save, ProductDto.class);
+    }
+
+    public void deleteProduct(Long id) {
+        if (!productRepository.existsById(id)) {
+            throw new ProductNotFoundException(PRODUCT_NOT_FOUND_MSG + id);
+        }
+        productRepository.deleteById(id);
+    }
+}
