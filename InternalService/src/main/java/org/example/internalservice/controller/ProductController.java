3fffe1c3fe851f65f6e1c7b45fd93704e20a2dc0
Quilsen <70715580+Quilsen@users.noreply.github.com>
2025-01-15 23:37:29 +0100

    (FEAT): add /filter endpoint

--- InternalService/src/main/java/org/example/internalservice/controller/ProductController.java
+++ InternalService/src/main/java/org/example/internalservice/controller/ProductController.java
@@ -5,6 +5,8 @@ import org.example.internalservice.dto.ProductCreateDto;
 import org.example.internalservice.dto.ProductDto;
 import org.example.internalservice.dto.ProductUpdateDto;
 import org.example.internalservice.service.ProductService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,9 +15,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @AllArgsConstructor
@@ -40,6 +44,15 @@ public class ProductController {
     @ResponseStatus(HttpStatus.OK)
     public List<ProductDto> getAllProducts() {
         return productService.getAllProducts();
+    }
+
+    @GetMapping("/filter")
+    @ResponseStatus(HttpStatus.OK)
+    public Page<ProductDto> filterProducts(@RequestParam(required = false) String name,
+                                        @RequestParam(required = false) String description,
+                                        @RequestParam(required = false) BigDecimal price,
+                                        Pageable pageable) {
+        return productService.filterProducts(pageable, name, description, price);
     }
 
     @PutMapping("/{id}")
