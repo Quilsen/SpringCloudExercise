4b9f9bc810b5f82dd867aba7833d63162dc68d51
Quilsen <70715580+Quilsen@users.noreply.github.com>
2025-01-16 17:07:13 +0100

    (FEAT): endpoint for user registration in keycloak

---
+++ ExternalService/src/main/java/org/example/externalservice/controller/AccountController.java
@@ -0,0 +1,24 @@
+package org.example.externalservice.controller;
+
+import lombok.AllArgsConstructor;
+import org.example.externalservice.dto.UserDto;
+import org.example.externalservice.service.AccountService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/account")
+public class AccountController {
+    private AccountService accountService;
+
+    @PostMapping("/register/parking-user")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void registerParkingUser(@RequestBody UserDto userDto) {
+        accountService.registerUser(userDto);
+    }
+}
