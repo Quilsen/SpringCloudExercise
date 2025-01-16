5f60b4c828779e58dd803ede0db17c2bfc0193ad
Quilsen <70715580+Quilsen@users.noreply.github.com>
2025-01-16 17:04:29 +0100

    (REFACTOR): created directory config

--- ExternalService/src/main/java/org/example/externalservice/config/RestTemplateConfig.java
+++ ExternalService/src/main/java/org/example/externalservice/config/RestTemplateConfig.java
@@ -1,4 +1,4 @@
-package org.example.externalservice;
+package org.example.externalservice.config;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
